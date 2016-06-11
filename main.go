0f22e3a2dc80eaa484a6bcbefc310d979a6bb134
Lars Kluge <l@larskluge.com>
2016-06-11 20:38:46 +0100

    Implement Ping

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"os"
+	"os/signal"
+	"syscall"
 
 	log "github.com/Sirupsen/logrus"
 	"github.com/golang/protobuf/proto"
@@ -30,20 +32,26 @@ func run(moduleName, cmd, address, kafkaBrokers string) {
 	log.Warn("Start module")
 	command = cmd
 	module := shared.NewModule(moduleName, false)
-	topic := module.KafkaTopicName("IO")
 
-	for {
-		key, value := bkconsumer.Consumer(topic)
-		in := &pbm.BinRequest{}
-		err := proto.Unmarshal(value, in)
-		check(err)
-		out, err := IO(in)
-		check(err)
-		msg, err := proto.Marshal(out)
-		check(err)
-		inboxTopic := "inbox." + key
-		bkproducer.Producer(key, inboxTopic, msg)
-	}
+	go work(module.KafkaTopicName("IO"))
+	go work(module.KafkaTopicName("Ping"))
+
+	wait := make(chan os.Signal, 1)
+	signal.Notify(wait, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
+	<-wait
+}
+
+func work(topic string) {
+	key, value := bkconsumer.Consumer(topic)
+	in := &pbm.BinRequest{}
+	err := proto.Unmarshal(value, in)
+	check(err)
+	out, err := IO(in)
+	check(err)
+	msg, err := proto.Marshal(out)
+	check(err)
+	inboxTopic := "inbox." + key
+	bkproducer.Producer(key, inboxTopic, msg)
 }
 
 func check(err error) {
