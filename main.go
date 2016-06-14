b3962727564cb250553cdfe91f99ab781c805944
Lars Kluge <l@larskluge.com>
2016-06-14 19:43:58 +0100

    Integrate kafka groups consumer

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 	"time"
 
@@ -10,7 +11,7 @@ import (
 	"github.com/golang/protobuf/proto"
 	pbm "github.com/larskluge/babl/protobuf/messages"
 	"github.com/larskluge/babl/shared"
-	"github.com/nneves/kafka-tools/bkconsumer"
+	"github.com/nneves/kafka-tools/bkconsumergroups"
 	"github.com/nneves/kafka-tools/bkproducer"
 )
 
@@ -35,18 +36,17 @@ func run(moduleName, cmd, address, kafkaBrokers string) {
 	module := shared.NewModule(moduleName, false)
 
 	go registerModule(moduleName)
-	go work(module.KafkaTopicName("IO"))
-	go work(module.KafkaTopicName("Ping"))
+	go work([]string{module.KafkaTopicName("IO"), module.KafkaTopicName("Ping")})
 
 	wait := make(chan os.Signal, 1)
 	signal.Notify(wait, syscall.SIGHUP, syscall.SIGINT, syscall.SIGTERM)
 	<-wait
 }
 
-func work(topic string) {
+func work(topics []string) {
 	for {
-		log.Infof("Work on topic %s", topic)
-		key, value := bkconsumer.Consumer(topic)
+		log.Infof("Work on topics %q", topics)
+		key, value := bkconsumergroups.ConsumerGroups(strings.Join(topics, ","))
 		in := &pbm.BinRequest{}
 		err := proto.Unmarshal(value, in)
 		check(err)
