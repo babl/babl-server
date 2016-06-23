9e560f6ba4ea2aba0cae819ae0b959ed99bafbe9
Lars Kluge <l@larskluge.com>
2016-06-23 18:07:13 +0100

    Remove deprecated function

--- kafka/utils.go
+++ kafka/utils.go
@@ -3,10 +3,7 @@ package kafka
 import (
 	"math/rand"
 	"strconv"
-	"strings"
 	"time"
-
-	"github.com/Shopify/sarama"
 )
 
 func getRandomID() string {
@@ -18,24 +15,6 @@ func getRandomID() string {
 func random(min, max int) int {
 	rand.Seed(time.Now().Unix())
 	return rand.Intn(max-min) + min
-}
-
-func getPartitions(c sarama.Consumer, topic, partitions string) ([]int32, error) {
-	if partitions == "all" || partitions == "" {
-		return c.Partitions(topic)
-	}
-
-	tmp := strings.Split(partitions, ",")
-	var pList []int32
-	for i := range tmp {
-		val, err := strconv.ParseInt(tmp[i], 10, 32)
-		if err != nil {
-			return nil, err
-		}
-		pList = append(pList, int32(val))
-	}
-
-	return pList, nil
 }
 
 func check(err error) {
