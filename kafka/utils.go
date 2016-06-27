11edb95cfdfa3c9d097dbdbafb74cb1f0830ab59
Nelson Neves <nelson.s.neves@gmail.com>
2016-06-27 13:55:40 +0100

    remove getRandomID(), no longer required - not thread-safe

--- kafka/utils.go
+++ kafka/utils.go
@@ -1,22 +1,5 @@
 package kafka
 
-import (
-	"math/rand"
-	"strconv"
-	"time"
-)
-
-func getRandomID() string {
-	randNbr := uint32(random(1, 999999))
-	randStr := strconv.FormatUint(uint64(randNbr), 10)
-	return randStr
-}
-
-func random(min, max int) int {
-	rand.Seed(time.Now().Unix())
-	return rand.Intn(max-min) + min
-}
-
 func check(err error) {
 	if err != nil {
 		panic(err)
