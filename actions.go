0289bf54b9e72d99665a7913ca6017c2d5b50e26
Lars Kluge <l@larskluge.com>
2016-07-01 16:09:25 +0100

    Support for async calculation & immediately response to request without having the whole calculation done

--- actions.go
+++ actions.go
@@ -15,94 +15,106 @@ import (
 
 func IO(in *pbm.BinRequest) (*pbm.BinReply, error) {
 	start := time.Now()
+	res := pbm.BinReply{Exitcode: 0}
 
-	cmd := exec.Command(command)
-	env := os.Environ()
-	cmd.Env = []string{} // {"FOO=BAR"}
-
-	vars := []string{}
-	for k, v := range in.Env {
-		cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", k, v))
-		vars = append(vars, k)
-	}
-	cmd.Env = append(cmd.Env, env...)
-	cmd.Env = append(cmd.Env, "BABL_VARS="+strings.Join(vars, ","))
-
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error("cmd.StdinPipe")
-	}
-	stdout, err := cmd.StdoutPipe()
-	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error("cmd.StdoutPipe")
-	}
-	stderr, err := cmd.StderrPipe()
-	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error("cmd.StderrPipe")
-	}
-	err = cmd.Start()
-	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error("cmd.Start")
+	done := make(chan bool, 1)
+	_, async := in.Env["BABL_ASYNC"]
+	if async {
+		done <- true
 	}
 
-	stdin.Write(in.Stdin)
-	stdin.Close()
-	outBytes, err := ioutil.ReadAll(stdout)
-	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error("ioutil.ReadAll[stdout]")
-	}
-	errBytes, err := ioutil.ReadAll(stderr)
-	if err != nil {
-		log.WithFields(log.Fields{"error": err}).Error("ioutil.ReadAll[stderr]")
-	}
+	go func() {
+		cmd := exec.Command(command)
+		env := os.Environ()
+		cmd.Env = []string{} // {"FOO=BAR"}
 
-	res := pbm.BinReply{
-		Stdout:   outBytes,
-		Stderr:   errBytes,
-		Exitcode: 0,
-	}
-
-	if err := cmd.Wait(); err != nil {
-		res.Exitcode = 255
-		if exiterr, ok := err.(*exec.ExitError); ok {
-			// The program has exited with an exit code != 0
-
-			// This works on both Unix and Windows. Although package
-			// syscall is generally platform dependent, WaitStatus is
-			// defined for both Unix and Windows and in both cases has
-			// an ExitStatus() method with the same signature.
-			if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
-				res.Exitcode = int32(status.ExitStatus())
-			}
-		} else {
-			log.WithFields(log.Fields{"error": err}).Error("cmd.Wait")
+		vars := []string{}
+		for k, v := range in.Env {
+			cmd.Env = append(cmd.Env, fmt.Sprintf("%s=%s", k, v))
+			vars = append(vars, k)
 		}
-	}
+		cmd.Env = append(cmd.Env, env...)
+		cmd.Env = append(cmd.Env, "BABL_VARS="+strings.Join(vars, ","))
 
-	status := 500
-	if res.Exitcode == 0 {
-		status = 200
-	}
+		stdin, err := cmd.StdinPipe()
+		if err != nil {
+			log.WithFields(log.Fields{"error": err}).Error("cmd.StdinPipe")
+		}
+		stdout, err := cmd.StdoutPipe()
+		if err != nil {
+			log.WithFields(log.Fields{"error": err}).Error("cmd.StdoutPipe")
+		}
+		stderr, err := cmd.StderrPipe()
+		if err != nil {
+			log.WithFields(log.Fields{"error": err}).Error("cmd.StderrPipe")
+		}
+		err = cmd.Start()
+		if err != nil {
+			log.WithFields(log.Fields{"error": err}).Error("cmd.Start")
+		}
 
-	elapsed := float64(time.Since(start).Seconds() * 1000)
+		stdin.Write(in.Stdin)
+		stdin.Close()
+		res.Stdout, err = ioutil.ReadAll(stdout)
+		if err != nil {
+			log.WithFields(log.Fields{"error": err}).Error("ioutil.ReadAll(stdout)")
+		}
+		res.Stderr, err = ioutil.ReadAll(stderr)
+		if err != nil {
+			log.WithFields(log.Fields{"error": err}).Error("ioutil.ReadAll(stderr)")
+		}
 
-	fields := log.Fields{
-		"stdin":       len(in.Stdin),
-		"stdout":      len(res.Stdout),
-		"stderr":      len(res.Stderr),
-		"exitcode":    res.Exitcode,
-		"status":      status,
-		"duration_ms": elapsed,
-	}
-	if status != 200 {
-		fields["error"] = string(res.Stderr)
-	}
-	l := log.WithFields(fields)
-	if status == 200 {
-		l.Info("call")
-	} else {
-		l.Error("call")
-	}
+		if err := cmd.Wait(); err != nil {
+			res.Exitcode = 255
+			if exiterr, ok := err.(*exec.ExitError); ok {
+				// The program has exited with an exit code != 0
+
+				// This works on both Unix and Windows. Although package
+				// syscall is generally platform dependent, WaitStatus is
+				// defined for both Unix and Windows and in both cases has
+				// an ExitStatus() method with the same signature.
+				if status, ok := exiterr.Sys().(syscall.WaitStatus); ok {
+					res.Exitcode = int32(status.ExitStatus())
+				}
+			} else {
+				log.WithFields(log.Fields{"error": err}).Error("cmd.Wait")
+			}
+		}
+
+		status := 500
+		if res.Exitcode == 0 {
+			status = 200
+		}
+
+		elapsed := float64(time.Since(start).Seconds() * 1000)
+
+		fields := log.Fields{
+			"stdin":       len(in.Stdin),
+			"stdout":      len(res.Stdout),
+			"stderr":      len(res.Stderr),
+			"exitcode":    res.Exitcode,
+			"status":      status,
+			"duration_ms": elapsed,
+		}
+		if status != 200 {
+			fields["error"] = string(res.Stderr)
+		}
+		if async {
+			fields["mode"] = "async"
+		} else {
+			fields["mode"] = "sync"
+		}
+		l := log.WithFields(fields)
+		if status == 200 {
+			l.Info("call")
+		} else {
+			l.Error("call")
+		}
+
+		done <- true
+	}()
+
+	<-done
 
 	return &res, nil
 }
