c962c4a9e62ef6b871faf53f0af8d58b6a1e3d68
Nelson Neves <nelson.s.neves@gmail.com>
2016-08-29 12:25:13 +0100

    fix: avoid kafka:ConsumeLastN crash if offset data is set but data was cleared from topic/parition

--- kafka/consumer.go
+++ kafka/consumer.go
@@ -1,6 +1,8 @@
 package kafka
 
 import (
+	"strings"
+
 	"github.com/Shopify/sarama"
 	log "github.com/Sirupsen/logrus"
 	. "github.com/larskluge/babl-server/utils"
@@ -56,11 +58,17 @@ func ConsumeLastN(client *sarama.Client, topic string, partition int32, lastn in
 		offset = 0
 	}
 
-	pc, err := consumer.ConsumePartition(topic, partition, offset)
-	if err != nil {
-		pc, err = consumer.ConsumePartition(topic, partition, sarama.OffsetNewest)
+	pc, err2 := consumer.ConsumePartition(topic, partition, offset)
+	if err2 != nil && strings.Contains(err2.Error(), "offset is outside the range") {
+		data := ConsumerData{Key: string(""), Value: []byte(""), Processed: make(chan string, 1)}
+		log.WithFields(log.Fields{"topic": topic, "partition": 0, "offset": offset, "key": "", "value size": 0}).Warn("Kafka Topic/Partition offset is outside the range")
+		ch <- &data
+		<-data.Processed
+		close(ch)
+		pc.Close()
+		return
 	}
-	Check(err)
+	Check(err2)
 	defer pc.Close()
 
 	for msg := range pc.Messages() {
