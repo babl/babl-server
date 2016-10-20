852ec37221032bf7ba0f5d0b4ad09bafa054c821
Lars Kluge <l@larskluge.com>
2016-10-20 13:16:18 +0200

    Clean up

--- kafka_meta.go
+++ kafka_meta.go
@@ -11,15 +11,12 @@ import (
 
 func listenToMetadata(client *sarama.Client) {
 	topic := bn.ModuleToTopic(ModuleName, true)
-	log.Debug("Consuming from module meta topic")
 	ch := make(chan *kafka.ConsumerData)
 	go kafka.Consume(client, topic, ch) // TODO read last 1,000 messages and place in cache upon start
 	for msg := range ch {
-		log.WithFields(log.Fields{"key": msg.Key}).Debug("Metadata received")
-
 		var meta pb.Meta
 		if err := proto.Unmarshal(msg.Value, &meta); err != nil {
-			log.Warn("Unknown meta data received")
+			log.WithError(err).Warn("Unknown meta data received")
 		} else {
 			if meta.Ping != nil {
 				handlePingRequest(meta.Ping)
@@ -28,8 +25,7 @@ func listenToMetadata(client *sarama.Client) {
 				handleCancelRequest(meta.Cancel)
 			}
 		}
-
 		msg.Processed <- "success"
 	}
-	panic("listenToModuleResponses: Lost connection to Kafka")
+	log.Panic("listenToMetadata: Lost connection to Kafka")
 }
